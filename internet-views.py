88207abdacfe2bdc2ee6522fca67382275f2dc77
hadapan <69392219+hadapan@users.noreply.github.com>
2020-08-15 20:27:58 +0700

    Create internet-views.py

---
+++ internet-views.py
@@ -0,0 +1,68 @@
+#Coded By Zidan-ID\King Mr_Z17
+
+#YouTube Mr_Z17
+
+#Vortex Indonesia
+
+import os,sys,time
+
+ # SILAHKAN COSTUMISASI SENDIRI YA BOSQ #
+
+def main():
+
+    time.sleep(1)
+
+    os.system ('clear')
+
+    print '[96m'
+
+    os.system ('figlet View YouTube')
+
+    print'[92m================================='
+
+    print'[97m Author : [96mJejak Internet '
+
+    print'[97m YouTube   : [96mJejak Internet '
+
+    print'[92m================================='
+
+    print'[92m++++++++++++ [97mM E N U [92m++++++++++++'
+
+    print'[92m[[97m1[92m] [97mView YouTube '
+
+    print'[92m[[91m0[92m] [91mExit '
+
+    gans = raw_input ('[97m==>[93m ')
+
+    if gans in ['1']:
+
+        time.sleep(1)
+
+        os.system ('git clone https://github.com/BladeKnife/ytviews ')
+
+        print 
+
+        print (' [+] Silakan ketik cd ytviews')
+
+        print (' [+] Silakan ketik python sub.py')
+
+        exit()
+
+    if gans in ['0']:
+
+        time.sleep(2)
+
+        exit()
+
+    else:
+
+        time.sleep(1)
+
+        print '[97m Pilih Yang Bener [92mBro . . .'
+
+        time.sleep(1)
+
+        main()
+
+main()
+
